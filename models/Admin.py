7593f5239761085095a19c8ff4845c54fa67b612
Hemang Choudhary <Coehemang@gmail.com>
2025-07-28 00:56:48 +0530

    Updated Readme

--- models/Admin.py
+++ models/Admin.py
@@ -12,21 +12,7 @@ class Admin(Base, UserMixin):
     email = Column(String(100), unique=True, nullable=False)
     
     parking_lots = relationship("ParkingLot", back_populates="admin", foreign_keys="ParkingLot.admin_id")
-    
-    @property
-    def is_authenticated(self):
-        return True
-        
-    @property
-    def is_active(self):
-        return True
-        
-    @property
-    def is_anonymous(self):
-        return False
-        
-    def get_id(self):
-        return str(self.id)
+
 
 
 
